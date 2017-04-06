e715957b4ab25683aa711ae4889aa95f50ccbfa0
CoolCode2 <wcole2222@gmail.com>
2017-04-05 21:23:31 -0600

    almost there

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,14 +4,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
 
 // How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require('./candyRouter');
+//we are importing router from other page
 
 app.use(bodyParser.json());
+app.use('/candies', candyRouter);
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
 
-app.listen(3000);
+console.log("candy server is running");
+app.listen(port);
+
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,62 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
-router = express.Router();
 
-var candies;
+var candies = [
+{"id":1,"name":"Chewing Gum","color":"Red"},
+{"id":2,"name":"Pez","color":"Green"},
+{"id":3,"name":"Marshmallow","color":"Pink"},
+{"id":4,"name":"Candy Stick","color":"Blue"}
+];
 
+var router = express.Router();
+
+var stringCandy = JSON.stringify(candies);
 //What would need to go into candies
 //in order to pass our first test?
 
+//1
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+	res.json(candies);
+	console.log('index is complete');
+	//res.JSON(candies);
 });
 
-// Fill out the rest of the routes here
+//2
+router.get('/:number', function(req,res){
 
-module.exports = router;
+	res.json(candies[req.params.number-1]);
+	console.log('step two get with an id complete');
+});
+
+//3 http://localhost:3000/candies POST
+// add a new candy
+router.post('/', function(req, res){
+	candies.push(req.body);
+	res.json(req.body);
+	console.log("post response is complete");
+});
+//4 is redundant
+//5 PUT http://localhost:3000/candies/3
+// update a candy
+router.put('/:number', function(req,res){
+	//req.body is the request coming in's body, storing it in the right candy arrya object
+	candies[req.params.number-1] = req.body;
+	res.json(candies[req.params.number-1]);
+	console.log('put sucessfully updated it');
+});
+
+router.delete('/:id', function(req,res){
+
+	candies.foreach(function(currentEl, index){
+		if(currentEl.id == params.id){
+			var indexLocation = req.params.id-1;
+			candies.splice(indexLocation, 1);
+			res.json('message d');
+		}
+	});
+	
+});
+
+
+module.exports = router; //connected to var router = express.Router();
